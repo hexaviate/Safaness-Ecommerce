bd889d405aa21660e3295fdf6aee36ed63a66a9b
qmenggg <azizyaqy@gmail.com>
2025-11-17 13:25:40 +0700

    width

--- resources/views/admin/layout/sidebar.blade.php
+++ resources/views/admin/layout/sidebar.blade.php
@@ -68,7 +68,7 @@
     <div class="navbar-vertical">
         <div class="px-4 py-5 d-flex justify-content-between align-items-center">
             <a href="{{ url('/') }}" class="navbar-brand">
-                <img src="{{ asset('images/safanes-logo.png') }}" alt="" />
+                <img src="{{ asset('images/safanes-logo.png') }}" alt="" width="180"/>
             </a>
             <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
         </div>
